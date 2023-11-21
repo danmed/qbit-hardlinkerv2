0abfbea1d7d8f32ce16fff86b351fe243a281170
danmed <danmed@gmail.com>
2023-11-21 12:01:24 +0000

    Update qbit-hardlinker.sh

--- qbit-hardlinker.sh
+++ qbit-hardlinker.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 # Get the path to the download and the name of the torrent
-download_path="$2"
-torrent_name="$1"
-torrent_label="$3"
 
-[ "${#torrent_label}" -gt 4 ] && torrent_label="$3" || torrent_label="SYNC"
+torrent_name="$1"
+download_path="$2"
+root_path="$3"
+torrent_label="$4"
+
+# If the torrent_label variable is 4 characters or more then all good
+# otherwise set the label to SYNC
+
+if [ -z "${torrent_label}" ];
+        then
+                torrent_label="SYNC"
+        fi
+
+#If the label is SEED then quit the script
 
 if ["$torrent_label" == "SEED"];
-    then
-        exit 1
-    fi
+        then
+                exit 1
+        fi
 
 # The destination folder
-destination="/home/user/downloads/qbittorrent/$torrent_label"
 
-# Single file torrents
-cp -l "$download_path/$torrent_name" "$destination/"
-# folders
-cp -lR "$download_path/$torrent_name" "$destination/"
+destination="/home4/foghorn/downloads/qbittorrent/$torrent_label"
+
+# Copy single file torrent and exit if successful
+
+cp -l "$download_path$torrent_name" "$destination/" && exit 1
+
+# Copy multi file (in a folder)
+
+cp -lR "$root_path" "$destination/"
