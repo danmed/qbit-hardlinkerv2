91bfbb5ca52af5a866b065b4049b7ab192a6d996
danmed <danmed@gmail.com>
2023-11-23 21:04:44 +0000

    Update qbit-hardlinker.sh

--- qbit-hardlinker.sh
+++ qbit-hardlinker.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 
-# Get the path to the download and the name of the torrent
+# Set default values if arguments are not provided
+torrent_name="${1:-}"
+download_path="${2:-}"
+root_path="${3:-}"
+torrent_label="${4:-SYNC}"
 
-torrent_name="$1"
-download_path="$2"
-root_path="$3"
-torrent_label="$4"
+# Quit the script if the label is "SEED"
+if [ "$torrent_label" == "SEED" ]; then
+    exit 1
+fi
 
-# If the torrent_label variable is 4 characters or more then all good
-# otherwise set the label to SYNC
-
-if [ -z "${torrent_label}" ];
-        then
-                torrent_label="SYNC"
-        fi
-
-#If the label is SEED then quit the script
-
-if ["$torrent_label" == "SEED"];
-        then
-                exit 1
-        fi
-
-# The destination folder
-
+# Set the destination folder
 destination="/home4/username/downloads/qbittorrent/$torrent_label"
 
-# Copy single file torrent and exit if successful
-
-cp -l "$download_path$torrent_name" "$destination/" && exit 1
-
-# Copy multi file (in a folder)
-
-cp -lR "$root_path" "$destination/"
+# Copy the torrent file or folder
+if [ -f "$download_path$torrent_name" ]; then
+    cp -l "$download_path$torrent_name" "$destination/" && exit 1
+elif [ -d "$root_path" ]; then
+    cp -lR "$root_path" "$destination/"
+fi
